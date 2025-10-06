0067e2bc99aea27625d27ca8b48dfe2474e1bab3
Pikachunoodles <colinkhangthai@gmail.com>
2025-10-05 22:13:53 -0700

    Added Player script
    Don't worry about how the project.godot just got added

---
+++ Player/player.gd
@@ -0,0 +1,10 @@
+extends CharacterBody2D
+
+#If you stumbled upon this file, this is for the advance programmers. 
+#please look away pretty please; this is far later
+#Also advance programmers: Hello!!! 
+#I'm only taking the most optimal answer: GLGL
+
+func _physics_process(delta: float) -> void:
+	pass
+	#this is your only hint and also use docs ;)
